787a6c200f81cb48bfde759f2cb5c618ef1fde69
An <an.t.thien@hiof.no>
2016-05-03 22:08:36 +0200

    Add Speaker and Session models

---
+++ MultiTenant/Models/Session.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MultiTenant.Models
+{
+    public class Session
+    {
+        public int Id { get; set; }
+
+        public int SessionId { get; set; }
+
+        public string Title { get; set; }
+
+        public string ShortDescription { get; set; }
+
+        public string Description { get; set; }
+
+        public Tenant Tenant { get; set; }
+
+        public List<Speaker> Speakers { get; set; }
+
+    }
+}
