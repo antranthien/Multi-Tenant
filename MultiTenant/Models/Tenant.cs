e260e6841843b437bca9b5ef6fc2c4629052a182
An <An@STONE-PC>
2016-05-03 22:00:18 +0200

    Add project files.

---
+++ MultiTenant/Models/Tenant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MultiTenant.Models
+{
+    public class Tenant
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string DomainName { get; set; }
+
+        //only  tenant is allowed to be true
+        public bool Default { get; set; }
+
+    }
+}
